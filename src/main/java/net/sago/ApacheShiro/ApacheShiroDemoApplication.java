eb76009875fc9b650865d1a81c00d696709c9a22
Julio <sago_78@hotmail.com>
2021-02-24 17:46:01 -0600

    initial code

---
+++ src/main/java/net/sago/ApacheShiro/ApacheShiroDemoApplication.java
@@ -0,0 +1,13 @@
+package net.sago.ApacheShiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheShiroDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheShiroDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/sago/ApacheShiro/ApacheShiroDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.sago.ApacheShiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheShiroDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
